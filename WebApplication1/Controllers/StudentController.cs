ced819459bbde9384da151ddb6b08972186ce81f
gururaju06 <109012258+gururaju06@users.noreply.github.com>
2022-07-19 12:54:37 +0530

    Add files via upload

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Data.OleDb;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Students()
+        {
+            //String connectionString = "Provider=MSOLEDBSQL;Data Source=192.168.100.56;Initial Catalog=LSG_TIMESHEET;Persist Security Info=True;User ID=Timesheet;Password=tsheet@1$;Pooling=False;providerName=System.Data.OleDb";
+            String connectionString = "Data Source=192.168.100.56;Initial Catalog=LSG_TIMESHEET;Persist Security Info=True;User ID=Timesheet;Password=tsheet@1$";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            String sql = "SELECT Top 10 * FROM Master_Employee where EMPLOYEEACTIVE=1 and EXEMPT='E' and SUB_DIVISION='LSG'";
+            SqlCommand cmd = new SqlCommand(sql, conn);            
+
+            var model = new List<Student>();
+            //using (SqlConnection conn = new SqlConnection(connectionString))
+            //{
+              //  conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var student = new Student();
+                student.Employee_No = rdr["Employee_Number"].ToString();
+                student.FirstName = rdr["Employee_First"].ToString();
+                student.LastName = rdr["Employee_Name"].ToString();
+ 
+                model.Add(student);
+            }
+            //}
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/Models/CustomerBAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CustomerBAL
+    {
+        public Customer GetCustomer()
+        {
+            Customer C = new Customer();
+            C.CustomerName = "Raju";
+            C.Address = "Bangalore";
+            C.Age = 50;
+            return C;
+        }
+
+    }
+}
